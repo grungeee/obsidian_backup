a622a51f03cdf9e80c6897c9c8c74a69c0f5805d
grungeee <sharapanikita@gmail.com>
2023-05-16 20:04:21 +0200

    vault backup: 2023-05-16 20:04:21
    Affected files:
    .obsidian/workspace.json
    WiFi-Share/Metallhelferprfüfung/convert_to_anki.py

---
+++ WiFi-Share/Metallhelferprfüfung/convert_to_anki.py
@@ -0,0 +1,18 @@
+def add_column_to_numbered_list(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    with open(file_path, 'w') as file:
+        for line in lines:
+            if line.strip().split('.')[0].isdigit():
+                number, rest = line.strip().split('.', 1)
+                new_line = f"{number}. \t{rest}\n"
+                file.write(new_line)
+            else:
+                file.write(line)
+
+# Usage example
+file_path = 'D:/Users/Public/Stuff/Notes/WiFi/WiFi-Share/Metallhelferprfüfung/1.md'
+
+add_column_to_numbered_list(file_path)
+:noh
